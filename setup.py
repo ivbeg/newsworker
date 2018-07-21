9729edd69c0deafaa53d0575f76eba250542a969
Иван Бегтин <ibegtin@gmail.com>
2018-07-21 10:37:22 +0300

    Initial commit

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     zip_safe=False,
     keywords='news parsing extraction feeds rss atom',
     classifiers=[
-        'Development Status :: 3 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
